4053f9a52f4b09bd30502de88dad65c250176a08
Norbert <norbert.niderla@gmail.com>
2019-11-15 23:44:27 +0100

    drobne zmiany w mainie

--- drft_main.m
+++ drft_main.m
@@ -1,33 +1,31 @@
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % % Bloki funkcjonalne (elementy projektu):
 % 1. konwersja mapy z Automated Driving Toolbox na zbior wektorow
-%             a) w najlepszym wypadku wystarczy zbior argumentow tych wektorow, 
-%                 jesli uzywany bedzie wektor znormalizowany
 % 2. konwersja zbioru wektorow sciezki na macierz promieni lukow po ktorych porusza sie pojazd
 % 3. konwersja zbioru wektorow sciezki na macierz katow natarcia pojazdu
 % 4. funkcja tworzaca macierz predkosci pojazdu na podstawie punktoww 2 i 3 oraz jakiegos wspolczynnika przyspieszenia
-% 5. blok liczacy material koncowy i koordynujacy reszte blokow na podstawie 
-%                    punktow 2,3,4. ( wlasciwie to na ten moment to juz tu jest)
-% 6 jakis frontend dla ambitnych ( wykres czy costam)
+% 5. blok liczacy material koncowy i koordynujacy reszte blokow na podstawie punktow 2,3,4.
+% 6. wykresy 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % 7. blok wykrywajacy understeer w wynikach
 % 8. blok wykrywajacy oversteer w wynikach
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all
 close all
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-global carLenght turnRadius size turnRadiusBack angleMatrix waypoints angleDiffMatrix;
 carLenght = 4;
-turnRadius = linspace(1,10,100);
-size = 100;
-
-[angleMatrix, angleDiffMatrix, waypoints] = routeMaking;
+[routeAngleMatrix, routeAngleDiffMatrix, waypoints] = routeMaking;
+size = length(waypoints);
 radiusMatrix = radiusCalculation;
+
+%tu wchodzi funkcja na kat natarcia
+%angleMatrix = costam;
+
+%tu wchodzi funkcja na predkosc pojazdu
+%frontVelocityMatrix = costam; 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -35,14 +33,13 @@ a = @(v, r) (v.^2)./r;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5
 
-frontVelocityMatrix = ones(1, size)*16;
-backVelocityMatrix = frontVelocityMatrix + frontVelocityMatrix.*(carLenght ./ turnRadius).*sin(angleMatrix);
-turnRadiusBack = turnRadius + (carLenght*sin(angleMatrix));
+backVelocityMatrix = frontVelocityMatrix + frontVelocityMatrix.*(carLenght ./ radiusMatrix).*sin(angleMatrix);
+radiusMatrixBack = radiusMatrix + (carLenght*sin(angleMatrix));
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-aFront = a(frontVelocityMatrix, turnRadius);
-aBack = a(backVelocityMatrix, turnRadiusBack);
+aFront = a(frontVelocityMatrix, radiusMatrix);
+aBack = a(backVelocityMatrix, radiusMatrixBack);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
